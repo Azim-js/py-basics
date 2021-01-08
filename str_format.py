8449eade0a8d21754ec53b35d19c1ceae624cd0b
Azim <66790056+Azim-js@users.noreply.github.com>
2021-01-08 17:27:36 +0530

    Add files via upload

---
+++ str_format.py
@@ -0,0 +1,11 @@
+print("string formating for python 2.0 and below")
+u_inp=input("enter your name: ")
+
+print("hello %s" %u_inp)
+
+
+
+print("string formating for python 3")
+u1_inp=input("enter user 2 name : ")
+
+print(f"heyo {u1_inp}")
