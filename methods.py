32f02f163eefedc9595eb04db0e155f38e89070c
Azim <66790056+Azim-js@users.noreply.github.com>
2021-01-12 13:06:38 +0530

    Add files via upload

---
+++ methods.py
@@ -0,0 +1,6 @@
+username="PYTHON3"
+print(username.lower())
+username1="python3"
+print(username1.upper())
+print("username is upper case",username.isupper(),"and tilte:",username.title())
+print("username1 is lowercase ",username1.islower())
