4b335482acdf119ab78e0eb6b807f292be8c0cb3
Azim <66790056+Azim-js@users.noreply.github.com>
2021-01-13 13:21:39 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,8 @@
+student_grades=[9,9,9,9,9]
+mysum=sum(student_grades)
+length=len(student_grades)
+average=mysum/length
+print("total:",mysum)
+c=student_grades.count(9)
+print("average:",average)
+print(c)
