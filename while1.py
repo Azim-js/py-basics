7452dc06cd3a734dfb090a5568f83eb128b9030d
Azim <66790056+Azim-js@users.noreply.github.com>
2021-01-13 13:20:19 +0530

    Add files via upload

---
+++ while1.py
@@ -0,0 +1,4 @@
+a=3
+
+while True and a>0 :
+    print(1,"hi i am in while loop")
---
+++ while2.py
@@ -0,0 +1,8 @@
+
+
+while True:
+    username=input("enter your username : ")
+    if username=="azim":
+        break
+    else:
+        continue
