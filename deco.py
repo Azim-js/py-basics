6bb4fee189bde29ef7a73c8f685eae50bece0ecf
Azim <66790056+Azim-js@users.noreply.github.com>
2021-03-08 18:35:46 +0530

    Update deco.py

--- deco.py
+++ deco.py
@@ -1,16 +1,16 @@
 def decor(func):
-	def inner(name):
-		if name=="Sunny":
-			print("Hello Sunny Bad Morning")
+        def inner(name):
+                if name=="Sunny":
+                        print("Hello Sunny Bad Morning")
 		else:
 			func(name)
 	return inner
 #calling of the decorator func
 @decor
 def wish(name):
-	print("Hello",name,"Good Morning")
+        print("Hello",name,"Good Morning")
 
-	wish("Azim")
-	wish("Hari")
-	wish("Sunny")
+wish("Azim")
+wish("Hari")
+wish("Sunny")
 	
